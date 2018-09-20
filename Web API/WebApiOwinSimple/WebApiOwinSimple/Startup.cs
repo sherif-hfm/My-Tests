8f87f94473a1b63891163dae77149d849d0ddd92
Sherif <sherif_hfm@yahoo.com>
2018-09-20 15:02:22 +0300

    ini commit

---
+++ Web API/WebApiOwinSimple/WebApiOwinSimple/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace WebApiOwinSimple
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // add web api
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            app.UseWebApi(config);
+        }
+    }
+}
