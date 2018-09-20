4641c16e9aa11cb99c26b28ff5db88e8c3735e86
Sherif <sherif_hfm@yahoo.com>
2018-09-20 15:20:02 +0300

    -

--- Web API/WebApiOwinSimple/WebApiHost/Program.cs
+++ Web API/WebApiOwinSimple/WebApiHost/Program.cs
@@ -13,18 +13,11 @@ namespace WebApiHost
     {
         static void Main(string[] args)
         {
-            string baseAddress = "http://localhost:9000/";
+            string baseAddress = "http://localhost:9001/";
 
             // Start OWIN host 
             using (WebApp.Start<Startup>(url: baseAddress))
             {
-                // Create HttpCient and make a request to api/values 
-                HttpClient client = new HttpClient();
-
-                var response = client.GetAsync(baseAddress + "api/values").Result;
-
-                Console.WriteLine(response);
-                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
                 Console.ReadLine();
             }
         }
